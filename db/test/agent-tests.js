e5509a298b3e421df2938d923e0c6f17e5c80f14
redesyef <ymauriciop@gmail.com>
2022-01-05 18:25:39 -0500

    Sinon

--- db/test/agent-tests.js
+++ db/test/agent-tests.js
@@ -1,15 +1,39 @@
 'use strict'
 
 const test = require('ava')
+const proxyrequire = require('proxyquire')
+const sinon = require('sinon')
 
 let db = null
+let AgentStub = null
+let sandbox = null
 let config = {
     loggin: function() {}
 }
+let MetricStub ={
+    belongsTo: sinon.spy()
+}
+
 test.beforeEach(async () => {
-    const setupDatabase = require('../')
+    sandbox = sinon.createSandbox()
+    AgentStub = {
+        hasMany: sandbox.spy()
+    }
+    const setupDatabase = proxyrequire('../',{
+        './models/agent' : () => AgentStub,
+        './models/metric' : () => MetricStub
+    })
     db = await setupDatabase(config)
 })
+test.afterEach(() => {
+    sandbox && sandbox.restore()
+  })
 test('Agent', t => {
    t.truthy(db.Agent, 'Agent service should exist')
+})
+test.serial('Setup', t => {
+    t.true(AgentStub.hasMany.called, 'AgentModel.hsMany was executed')
+    t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+    t.true(MetricStub.belongsTo.called, 'Metric.Model.belongTo was executed')
+    t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
 })
